3c7498fc6b09a459deea417996f3e4f5f0f04c1e
Ezra Yoanda Nicolas <ezrayoandanicolas@gmail.com>
2026-08-05 22:34:43 +0700

    fix: history transaksi - deep convert API response so nested user/bank fields render correctly

--- app/Http/Controllers/HistoryTransaksiController.php
+++ app/Http/Controllers/HistoryTransaksiController.php
@@ -9,10 +9,10 @@ class HistoryTransaksiController extends FrontendController
     public function index()
     {
         $resp = $this->apiGet('transactions/deposits-completed');
-        $deposits = collect($resp['data']['transactions']['data'] ?? [])->map(fn($d) => (object) $d);
+        $deposits = collect($resp['data']['transactions']['data'] ?? [])->map(fn($d) => json_decode(json_encode($d)));
 
         $respW = $this->apiGet('transactions/withdraws-completed');
-        $withdraws = collect($respW['data']['transactions']['data'] ?? [])->map(fn($w) => (object) $w);
+        $withdraws = collect($respW['data']['transactions']['data'] ?? [])->map(fn($w) => json_decode(json_encode($w)));
 
         return view('backoffice.histori_transaksi.histori_transaksi', compact('deposits', 'withdraws'));
     }
@@ -20,14 +20,14 @@ class HistoryTransaksiController extends FrontendController
     public function getDepositHistory()
     {
         $resp = $this->apiGet('transactions/deposits-completed');
-        $deposits = $resp['data']['transactions']['data'] ?? [];
+        $deposits = collect($resp['data']['transactions']['data'] ?? [])->map(fn($d) => json_decode(json_encode($d)));
         return response()->json($deposits);
     }
 
     public function getWithdrawHistory()
     {
         $resp = $this->apiGet('transactions/withdraws-completed');
-        $withdraws = $resp['data']['transactions']['data'] ?? [];
+        $withdraws = collect($resp['data']['transactions']['data'] ?? [])->map(fn($w) => json_decode(json_encode($w)));
         return response()->json($withdraws);
     }
 
